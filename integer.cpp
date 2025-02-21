36b7c9e37e7a1d0cc2f094840ffff51fd86836be
Faustine Lafitte <faustine.lafitte@etu.minesparis.psl.eu>
2025-02-21 15:11:00 +0100

    Exo Integer

---
+++ integer.cpp
@@ -0,0 +1,14 @@
+class Integer {
+    int value{};
+    void incr(){
+        value=value+1;
+    }
+    void 
+}; // NE PAS OUBLIER LE POINT VIRGULE !!!
+
+int main(){
+    Integer i;
+    Integer j=i;
+
+    return 0;
+}
